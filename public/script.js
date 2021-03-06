3c2d42141290c759d551b27c7b79127b03359e74
jmcint21 <50252036+jmcint21@users.noreply.github.com>
2021-03-05 19:03:38 -0500

    Updated JS

--- public/script.js
+++ public/script.js
@@ -3,19 +3,19 @@ const locations = [];
 
 fetch(endpoint)
     .then(blob => blob.json())
-    .then(data => VRDisplayCapabilities.push(...data))
+    .then(data => locations.push(...data))
 
 function findMatches(wordToMatch, locations) {
     return locations.filter(place => {
     // here we need to figure out if the zip matches what was searched
-    const regex = new RegExp(wordToMatch, 'gi')
-    return place.zip.match(regex)
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex)
 });
 }
 
-function numberWithCommas(x) {
-    return locations.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+//function numberWithCommas(x) {
+//    return locations.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+//}
 
 function displayMatches() {
     const matchArray = findMatches(this.value, locations);
@@ -23,15 +23,18 @@ function displayMatches() {
         return `
             <li>
                 <span class="name">${place.name}</span>
-                <span class="category">${categoryName}</span>
-                <span class="address">${place.address_line_1}</span>
-                <span class="city">${place.city}</span>
-                <span class="zip">${place.zip}</span>
+//                <span class="category">${place.categoryName}</span>
+//                <span class="address">${place.address_line_1}</span>
+//                <span class="city">${place.city}</span>
+//                <span class="zip">${place.zip}</span>
             </li>
         `;
 }).join('');
-suggestions.innerHTML = html;
+suggestions.innerHTML += html;
 }
 
-search.addEventListener('change', displayMatches);
-search.addEventListener('keyup', displayMatches);
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggesttions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
